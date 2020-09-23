174e92512c5e66510d2455d11b5fa1ea6c1b5e18
siolinm <siolinm@hotmail.com>
2020-09-23 20:13:32 -0300

    Create files kinetic

---
+++ pair/kinetic/util.h
@@ -0,0 +1,16 @@
+#ifndef _UTIL_H
+#define _UTIL_H
+
+enum Cert_types{
+    horizontal_cert,
+    up_cert,
+    down_cert
+};
+
+typedef struct Cert{
+    /* certificate type */
+    int type;
+    
+} Cert;
+
+#endif
